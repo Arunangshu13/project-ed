733d924ab0c17e992762ce946d9f5c2185da5515
Arunangshu Biswas <arunangshu.biswas.ab16@gmail.com>
2022-03-06 15:42:17 +0530

    commit from my alter ego

---
+++ driver.py
@@ -0,0 +1,7 @@
+def begin_encoding():
+    """
+        this function is responsible for initiating an encoding process followed by subsequent algorithm
+    """
+    print("process started ... ")
+    encoded_messege=""
+    return encoded_messege
